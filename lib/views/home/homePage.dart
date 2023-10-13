082b3fbe7fb6d4e19e725b5f34559d061e50f111
AzZaya0 <ajaya.ajyol112@gmail.com>
2023-10-13 14:12:30 +0545

    firebase init

--- lib/views/home/homePage.dart
+++ lib/views/home/homePage.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      body: Container(
+        height: screenHeight,
+        width: screenWidth,
+        
+      ),
+    );
   }
 }
